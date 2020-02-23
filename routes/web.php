958d57a7dfebdb87b00d9b7840100921ca2aa647
Borhaneddine Guemidi <guemidiborhane@gmail.com>
2020-02-23 03:57:14 +0100

    Add treafik

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/home', function () {
+  return redirect()->route('chatter.home');
+});
