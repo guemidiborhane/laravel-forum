63acc163a190b5936f011f9a6d4493292aafd0e1
Borhaneddine Guemidi <guemidiborhane@gmail.com>
2020-02-23 04:19:02 +0100

    Fix Countable on php7.2

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
+
+if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
+    error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+}
 
 Route::get('/', function () {
     return view('welcome');
